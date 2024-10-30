f719203529727313428f7719e3f2a8814fe60729
Rehan <rehannags@yahoo.com>
2024-10-30 21:16:54 +0400

    added different models and comparisons

--- pred.py
+++ pred.py
@@ -48,8 +48,49 @@ X_test = scaler.fit_transform(X_test)
 #  logistic regression model
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import pickle
+
+import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
 lr_model = LogisticRegression(random_state=42)
 lr_model.fit(X_train, y_train)
 
 lr_predictions = lr_model.predict(X_test)
 lr_accuracy = accuracy_score(y_test, lr_predictions)
+
+def evaluate_and_save_model(model, X_train, X_test, y_train, y_test, filename):
+   model.fit(X_train, y_train)
+   y_pred = model.predict(X_test)
+   accuracy = accuracy_score(y_test, y_pred)
+   print(f"{model.__class__.__name__} Accuracy: {accuracy: .4f}")
+   print(f"\nClassification Report:\n{classification_report(y_test, y_pred)}")
+   print("-----------")
+
+   with open(filename, "wb") as file:
+      pickle.dump(model, file)
+   
+   print(f"Model saved as {filename}")
+
+# Check and compare models and accuracy
+xgb_model = xgb.XGBClassifier(random_state=42)
+evaluate_and_save_model(xgb_model, X_train, X_test, y_train, y_test, "xgb_model.pkl")
+
+dt_model = DecisionTreeClassifier(randome_state=42)
+evaluate_and_save_model(dt_model, X_train, X_test, y_train, y_test, "dt_model.pkl")
+
+rf_model = RandomForestClassifier(random_state=42)
+evaluate_and_save_model(dt_model, X_train, X_test, y_train, y_test, "dt_model.pkl")
+
+nb_model = GaussianNB()
+evaluate_and_save_model(nb_model, X_train, X_test, y_train, y_test, "nb_model.pkl")
+
+knn_model = KNeighborsClassifier()
+evaluate_and_save_model(knn_model, X_train, X_test, y_train, y_test, "knn_model.pkl")
+
+svm_model = SVC(random_state=42)
+evaluate_and_save_model(svm_model, X_train, X_test, y_train, y_test, "svm_model.pkl")
